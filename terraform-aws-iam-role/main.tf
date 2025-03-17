e125a411143e56661aaf8e295f632cff6d3dd82a
Ronan Delacroix <ronan.delacroix@gmail.com>
2025-03-17 18:51:25 +0100

    fix: fix a bug and simplify a few things.

--- terraform-aws-iam-role/main.tf
+++ terraform-aws-iam-role/main.tf
@@ -1,12 +1,7 @@
 locals {
-  iam_role_name = var.iam_role_name != null ? var.iam_role_name : "${var.prefix}-iam-role-${random_id.uniq.hex}"
-  external_id   = var.external_id != null ? var.external_id : "cxm:${random_id.uniq.hex}"
-  principal     = var.cxm_role_name != null ? "arn:aws:iam::${var.cxm_aws_account_id}:role/${var.cxm_role_name}" : "arn:aws:iam::${var.cxm_aws_account_id}:root"
+  principal = var.cxm_role_name != null ? "arn:aws:iam::${var.cxm_aws_account_id}:role/${var.cxm_role_name}" : "arn:aws:iam::${var.cxm_aws_account_id}:root"
 }
 
-resource "random_id" "uniq" {
-  byte_length = 4
-}
 
 data "aws_iam_policy_document" "cxm_assume_role_policy" {
   count   = var.dry_run ? 0 : 1
@@ -24,14 +19,14 @@ data "aws_iam_policy_document" "cxm_assume_role_policy" {
     condition {
       test     = "StringEquals"
       variable = "sts:ExternalId"
-      values   = [local.external_id]
+      values   = [var.external_id]
     }
   }
 }
 
 resource "aws_iam_role" "cxm_iam_role" {
   count                = var.dry_run ? 0 : 1
-  name                 = local.iam_role_name
+  name                 = var.iam_role_name
   assume_role_policy   = data.aws_iam_policy_document.cxm_assume_role_policy[count.index].json
   permissions_boundary = var.permission_boundary_arn
   tags                 = var.tags
--- terraform-aws-iam-role/variables.tf
+++ terraform-aws-iam-role/variables.tf
@@ -1,38 +1,22 @@
-variable "dry_run" {
-  type        = bool
-  default     = false
-  description = "Setting dry_run to `true` will prevent the module from creating new resources."
-}
-
 variable "iam_role_name" {
   type        = string
-  default     = null
-  description = "The IAM role name"
+  description = "The IAM role name to create."
+}
+
+variable "external_id" {
+  type        = string
+  description = "External ID provided by Cloud ex Machina to configure the role."
 }
 
 variable "cxm_aws_account_id" {
-  type = string
-  # Change this to a multi-tenant account for default PoVs
-  default     = "596683793973"
-  description = "The Cloud ex Machina AWS account that the IAM role will grant access."
-}
-
-variable "prefix" {
   type        = string
-  default     = "cxm"
-  description = "Prefix to use for most resources created by this module."
+  description = "The Cloud ex Machina AWS account that the IAM role will grant access."
 }
 
 variable "cxm_role_name" {
   type        = string
   default     = null
-  description = "Name of the IAM role in the Cloud ex Machina AWS account that will assume this execution role"
-}
-
-variable "external_id" {
-  type        = string
-  default     = null
-  description = "External ID provided by Cloud ex Machina to configure the role."
+  description = "Name of the IAM role in the Cloud ex Machina AWS account that will assume this execution role. If null, root will be used."
 }
 
 variable "permission_boundary_arn" {
@@ -45,4 +29,10 @@ variable "tags" {
   type        = map(string)
   description = "A map/dictionary of Tags to be assigned to created resources."
   default     = {}
+}
+
+variable "dry_run" {
+  type        = bool
+  default     = false
+  description = "Setting dry_run to `true` will prevent the module from creating new resources."
 }
